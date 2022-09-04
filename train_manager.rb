993c24f378d3dcad4486dc9598aa2fb7be91c5e5
R-a-m-o-s-c-h <107832251+R-a-m-o-s-c-h@users.noreply.github.com>
2022-09-04 09:07:26 +0300

    Update train_manager.rb

--- train_manager.rb
+++ train_manager.rb
@@ -1,52 +1,73 @@
-require './train_manager/train_processor.rb'
-require './train_manager/city_processor.rb'
+require_relative 'train_processor.rb'
+require_relative 'city_processor.rb'
 
-$status = [
-    {"Omsk" => ["Sapsan", "Emsam"]},
-    {"Novosibirsk" => ["Baklan"]},
-    {"Pavlodar" => []},
-    {"Stavropol" => ["Svekla"]}
-]
+class TrainManager
+  CHOISE_MESSAGE = "Выберите необходимую процедуру:
+  1 - переместить поезд
+  2 - создать новый поезд
+  3 - создать новый город
+  4 - завершить программу"
 
-$choise_message = "Выберите необходимую процедуру:
-1 - переместить поезд
-2 - создать новый поезд
-3 - создать новый город
-4 - завершить программу"
+  def initialize(status)
+    @status = status
+    @train_processor = create_train_processor
+    @city_processor = create_city_processor
+  end
 
-puts
-puts "Добро пожаловать в менеджер поездов."
-puts
+  def call
+    print_welcome
 
-trainProcessor = TrainProcessor.new
-cityProcessor = CityProcessor.new
+    user_choise = 0
 
-userChoise = 0
+    while user_choise do
+      print_status
 
-while userChoise do
-  puts "На данный момент поезда находятся в следующих городах:"
-  puts
-  puts $status
-  puts
-  puts $choise_message
+      user_choise = gets.chomp.to_i
 
-  userChoise = gets.chomp.to_i
+      if user_choise == 1
+        @train_processor.move_train # 1
+      elsif user_choise == 2
+        @train_processor.train_creation # 2
+      elsif user_choise == 3
+        @city_processor.city_creation # 3
+      elsif user_choise == 4
+        abort "Программа завершена."
+      else
+        "Вы не ввели значение или ввели неверное. Попробуйте еще раз."
+      end
+    end
+  end
 
-  if userChoise == 1
-    trainProcessor.move_train # 1
-  elsif userChoise == 2
-    trainProcessor.train_creation # 2
-  elsif userChoise == 3
-    cityProcessor.city_creation # 3
-  elsif userChoise == 4
-    abort "Программа завершена."
-  else
-    "Вы не ввели значение или ввели неверное. Попробуйте еще раз."
+  private
+
+  def print_welcome
+    puts
+    puts "Добро пожаловать в менеджер поездов."
+    puts
+  end
+
+  def print_status
+    puts "На данный момент поезда находятся в следующих городах:"
+    puts
+    puts @status
+    puts
+    puts CHOISE_MESSAGE
+  end
+
+  def create_train_processor
+    TrainProcessor.new(@status)
+  end
+
+  def create_city_processor
+    CityProcessor.new(@status)
   end
 end
 
+initial_state = [
+  {"Omsk" => [["Sapsan", 1], ["Emsam", 1]]},
+  {"Novosibirsk" => [["Baklan", 1]]},
+  {"Pavlodar" => []},
+  {"Stavropol" => [["Svekla", 1]]}
+]
 
-
-
-
-
+TrainManager.new(initial_state).call
